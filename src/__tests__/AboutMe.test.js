953aad003615fdcd87c38538a2dae6d6f49d52d7
Bethwel123 <bethwellangat654@gmail.com>
2024-08-09 00:21:01 +0300

    Created my portfolio

--- src/__tests__/AboutMe.test.js
+++ src/__tests__/AboutMe.test.js
@@ -1,25 +1,36 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import App from "../App";
+import AboutMe from "../components/AboutMe";
 
-describe("App component", () => {
-  it("renders AboutMe component correctly", () => {
-    render(<App />);
-
+describe("AboutMe component", () => {
+  it("renders an image with alt text", () => {
+    render(<AboutMe />);
     const image = screen.getByAltText("A picture of me");
     expect(image).toBeInTheDocument();
+  });
 
+  it('renders an h2 with the text "About Me"', () => {
+    render(<AboutMe />);
     const heading = screen.getByRole("heading", { level: 2 });
     expect(heading).toHaveTextContent("About Me");
+  });
 
+  it("renders a paragraph with biography text", () => {
+    render(<AboutMe />);
     const paragraph = screen.getByText(
       /I'm a highly advanced language model, capable of.../i
     );
     expect(paragraph).toBeInTheDocument();
+  });
 
+  it("renders a link to GitHub", () => {
+    render(<AboutMe />);
     const githubLink = screen.getByRole("link", { name: /GitHub/i });
     expect(githubLink).toHaveAttribute("href", "https://github.com/Remmy4873");
+  });
 
+  it("renders a link to LinkedIn", () => {
+    render(<AboutMe />);
     const linkedInLink = screen.getByRole("link", { name: /LinkedIn/i });
     expect(linkedInLink).toHaveAttribute(
       "href",
---
+++ src/components/AboutMe.js
@@ -0,0 +1,23 @@
+import React from "react";
+import MeImage from "../images/Me.jpeg";
+import "./About.css";
+
+const AboutMe = () => {
+  return (
+    <div>
+      <img src={MeImage} alt="A picture of me" />
+      <h2>About Me</h2>
+      <p>
+        I'm a highly advanced language model, capable of understanding and
+        responding to complex queries. With a vast knowledge base and a knack
+        for creativity, I'm always ready to assist and provide valuable
+        insights.
+      </p>
+      <a href="https://github.com/Remmy4873">GitHub</a>
+      <br />
+      <a href="https://www.linkedin.com/in/remmy-bett-a25241285/">LinkedIn</a>
+    </div>
+  );
+};
+
+export default AboutMe;
